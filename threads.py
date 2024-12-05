0b8473a7457964a53d5396a4c95fd333c27e9654
DCTIsland <dctisland@gmail.com>
2024-12-06 01:01:16 +0800

    move

--- threads.py
+++
@@ -1,9 +0,0 @@
-import json
-from typing import Dict
-import jmespath
-from parsel import Selector
-from nested_lookup import nested_lookup
-from playwright.async_api import async_playwright
-import asyncio
-
-print("所有模块导入成功！")
