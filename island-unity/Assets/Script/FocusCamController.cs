6bb345a0f7112aa140bffc0a5a677ed46528ba91
SY <pkid678@gmail.com>
2025-05-15 13:13:55 +0800

    feat: modify cam move
    feat: modify cam move

--- island-unity/Assets/Script/FocusCamController.cs
+++ island-unity/Assets/Script/FocusCamController.cs
@@ -9,21 +9,33 @@ public class FocusCamController : MonoBehaviour
     float verticalInput;
     float speed = 1f;
 
+    float yaw = 180f;
+    public float mouseSensitivity = 3f;
+
     void Update()
     {
-        if(!allowMove)
+        if (!allowMove)
             return;
-        
+
+        //rotation
+        if (Input.GetMouseButton(1))
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            transform.rotation = Quaternion.Euler(35, yaw, 0);
+        }
+
+        //movement
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        if(horizontalInput != 0 && verticalInput != 0)
+        if (horizontalInput != 0 && verticalInput != 0)
         {
             horizontalInput *= 0.7f;
             verticalInput *= 0.7f;
         }
 
-        Vector3 moveDirection = new Vector3(-horizontalInput, 0, -verticalInput);
+        Vector3 inputDirection = new Vector3(horizontalInput, 0f, verticalInput);
+        Vector3 moveDirection = Quaternion.Euler(0f, yaw, 0f) * inputDirection.normalized;
         transform.position += moveDirection * speed * Time.deltaTime;
     }
 }
